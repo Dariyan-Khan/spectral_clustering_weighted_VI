f89996f36e822154ddb16388fa40837227f079dc
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2024-04-22 09:25:24 +0100

    updated prior for phi

--- phi_dist.py
+++ phi_dist.py
@@ -2,14 +2,14 @@ import numpy as np
 
 class phi():
 
-    def __init__(self, prior, K):
-        self.prior_params = prior
+    def __init__(self, α_prior, K):
+        self.prior_params = [α_prior/K for _ in range(K)]
         self.params = None
         self.K = K # number of classes
 
         assert len(self.prior_params) == self.K, "Number of classes must match number of prior parameters"
     
-    def weighted_vi(self, z_list, phi_vi_list): # Need to pass in a list of z distributions for each point
+    def vi(self, z_list, phi_vi_list): # Need to pass in a list of z distributions for each point
 
         assert len(z_list) == len(phi_vi_list), "Number of z distributions must match number of phi_vi distributions"
 
