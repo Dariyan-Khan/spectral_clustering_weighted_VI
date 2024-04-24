1bc70c8c0f6e15bb02a41e6b2c512602d192b775
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2024-04-24 08:56:34 +0100

    expectation of sigma done

---
+++ sigma_inv.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def sigma_inv_approx(sigma_star, γ):
+
+    d = sigma_star.d
+
+    """
+    Each matrix will be of the form
+    | A B|
+    | B C|
+    """
+
+    def Sigma_expectation(sigma_star, γ):
+
+        A = (sigma_star.scale / (sigma_star.dof - d)) + γ.cov + γ.mean @ γ.mean.T
+        B = γ.mean
+        C = 1.0
+
+        return np.block([[A, B], [B.T, C]])
+
+
+    def Sigma_sq_expectation_(sigma_star, γ):
+         
