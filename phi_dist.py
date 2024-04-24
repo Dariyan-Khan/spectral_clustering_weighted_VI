72dc58afb0ec786f2af4a587b87a24e337c515ce
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2024-04-24 08:19:05 +0100

    corrected phi_dist

--- phi_dist.py
+++ phi_dist.py
@@ -7,11 +7,9 @@ class phi():
         self.conc = None
         self.K = K # number of classes
 
-        assert len(self.prior_params) == self.K, "Number of classes must match number of prior parameters"
+        assert len(self.prior_conc) == self.K, "Number of classes must match number of prior parameters"
     
     def vi(self, z_vi_list): # Need to pass in a list of z distributions for each point
-
-        assert len(z_vi_list) == len(phi_vi_list), "Number of z distributions must match number of phi_vi distributions"
 
         exp_els_in_cluster = self.prior_conc
         for z in z_vi_list:
