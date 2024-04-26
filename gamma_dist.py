a73f53ea81fbd4e5d99955fbe42a8234a3885832
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2024-04-26 09:32:26 +0100

    sigma inversee completed

--- gamma_dist.py
+++ gamma_dist.py
@@ -52,6 +52,8 @@ class mu():
         self.corr = self.cov / np.sqrt(np.outer(np.diag(self.cov), np.diag(self.cov)))
 
         self.triple_gamma = self.three_gama()
+
+        self.quad_gamma = self.quadruple_gamma()
     
 
     def outer_prod(self):
@@ -59,7 +61,7 @@ class mu():
 
 
     def three_gamma(self):
-        col_vars = np.tile(np.diag(self.cov).reshape(1, self.dim), (self.dim, 1))
+        col_vars = np.tile(np.diag(self.cov).reshape(1, self.dim), (self.dim, 1)) # if we change rows, should stay the same 
         A_mat  = self.cov / col_vars
 
         np.fill_diagonal(A_mat, 0)
@@ -67,8 +69,8 @@ class mu():
         M_2_vec = np.diag(self.cov) + self.mean**2
         M_3_vec = self.mean**3 + 3 * self.mean * np.diag(self.cov)
 
-        row_mu = np.tile(self.mean.reshape(1,self.dim), (self.dim,1))
-        col_mu = row_mu.T
+        col_mu = np.tile(self.mean.reshape(1,self.dim), (self.dim,1))  # if we change rows, should stay the same 
+        row_mu = col_mu.T  # if we change cols, should stay the same 
 
         B_mat = row_mu - col_mu*A_mat
 
@@ -80,16 +82,16 @@ class mu():
     def quadruple_gamma(self):
         quad_mat =  np.zeros((self.dim, self.dim))
 
-        col_vars = np.tile(np.diag(self.cov).reshape(1, self.dim), (self.dim, 1))
-        row_vars = col_vars.T
+        col_vars = np.tile(np.diag(self.cov).reshape(1, self.dim), (self.dim, 1)) # if we change rows, should stay the same 
+        row_vars = col_vars.T # if we change cols, should stay the same 
 
         A_mat  = self.cov / col_vars
         np.fill_diagonal(A_mat, 0)
 
         A_sq_mat = A_mat**2
 
-        row_mu = np.tile(self.mean.reshape(1,self.dim), (self.dim,1))
-        col_mu = row_mu.T
+        col_mu = np.tile(self.mean.reshape(1,self.dim), (self.dim,1))
+        row_mu = row_mu.T
 
         B_mat = 2 * A_mat * (row_mu - col_mu*A_mat)
         np.fill_diagonal(B_mat, 0)
@@ -101,8 +103,6 @@ class mu():
         M_3_vec = self.mean**3 + 3 * self.mean * np.diag(self.cov)
         M_4_vec = self.mean**4 + 6 * self.mean**2 * np.diag(self.cov) + 3 * np.diag(self.cov)**2
 
-        M_4_vec + 4 * (A_sq_mat @ M_4_vec) + (B_mat @ M_3_vec) + (C_mat @ M_2_vec)
-
         # diagonal terms
 
         np.fill_diagonal(quad_mat, M_4_vec + 4 * (A_sq_mat @ M_4_vec) + (B_mat @ M_3_vec) + (C_mat @ M_2_vec))
@@ -110,7 +110,67 @@ class mu():
 
         # off-diagonal terms
 
+        B_mat_off_diag = col_mu - (row_mu * (A_mat / row_vars))
+
+        P_tensor = np.zeros((self.dim, self.dim, self.dim))
+        Q_tensor = np.zeros((self.dim, self.dim, self.dim))
+
+        for i in range(self.dim):
+            for j in range(self.dim):
+                for k in range(self.dim):
+                    if i == j or k == i or k == j:
+                        continue
+
+                    P_tensor[i, j, k] = self.mean[i] * np.sqrt(self.cov[i,i]) * (self.corr[i, k] - self.corr[j,k] * self.corr[i,j]) / \
+                                        (np.sqrt(self.cov[k,k]) * (1-self.corr[j,k]**2))
+
+                    Q_tensor[i, j, k] = (self.mean[j]**2 + self.cov[j,j]) * \
+                                        (
+                                            np.sqrt(self.cov[i,i]) * (self.corr[i, j] - self.corr[j,k] * self.corr[i,k]) / \
+                                         (np.sqrt(self.cov[j,j]) * (1-self.corr[j,k]**2))
+                                        )
+                    
+                    Q_tensor[i, j, k] += self.mean[j] * (
+                        self.mean[i] + (
+                            (
+                                np.sqrt(self.cov[i,i]) * np.sqrt(self.cov[j,j]) * \
+                                self.mean[k] * (self.corr[i,j] * self.corr[j,k] - self.corr[i,k]) + \
+                                np.sqrt(self.cov[i,i]) * np.sqrt(self.cov[k,k]) * \
+                                self.mean[j] * (self.corr[i,k] * self.corr[j,k] - self.corr[i,j])
+                            ) / \
+                                ((1 - self.corr[j,k]**2) * np.sqrt(self.cov[j,j]) * np.sqrt(self.cov[k,k]))
+                    )
+                    )
+
+        for i in range(self.dim):
+            for j in range(self.dim):
+                if i == j:
+                    continue
+
+                quad_mat[i,j] = A_mat[i,j]*M_4_vec[i] + B_mat_off_diag[i,j] * M_3_vec[i] + \
+                                A_mat[j,i]*M_4_vec[j] + B_mat_off_diag[j,i] * M_3_vec[j]
+                
+                for k in range(self.dim):
+                    if k == i or k == j:
+                        continue
+
+                    quad_mat[i,j] += P_tensor[i,j,k] * M_3_vec[k] + Q_tensor[i,j,k] * M_2_vec[k]
         
+
+        return quad_mat
+
+
+                
+                    
+                    
+
+
+
+                   
+
+                
+
+
 
 
 
--- sigma_inv.py
+++ sigma_inv.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def sigma_inv_approx(sigma_star, γ):
+def sigma_inv_approx(sigma_star, γ, α=1): # α is the term added for convergence purposes
 
     d = sigma_star.d
 
@@ -10,7 +10,7 @@ def sigma_inv_approx(sigma_star, γ):
     | B C|
     """
 
-    def Sigma_expectation(sigma_star, γ):
+    def Sigma_expectation(sigma_star, γ, α=1):
 
         A = sigma_star.first_moment() + γ.cov + γ.mean @ γ.mean.T
         B = γ.mean
@@ -19,7 +19,23 @@ def sigma_inv_approx(sigma_star, γ):
         return np.block([[A, B], [B.T, C]])
 
 
-    def Sigma_sq_expectation_(sigma_star, γ):
+    def Sigma_sq_expectation(sigma_star, γ):
 
+        A = sigma_star.second_moment + sigma_star.first_moment @ γ.outer_product + \
+            γ.outer_product @ sigma_star.first_moment + γ.quad_gamma
         
+        B = sigma_star.first_moment @ γ.mean + γ.triple_gamma + γ.mean
+
+        C = np.trace(γ.outer_product) + 1.0
+
+        return np.block([[A, B], [B.T, C]])
+    
+    
+
+    return 3 * α * np.eye(d) - 3 * α**2 * Sigma_expectation(sigma_star, γ) + α**2 * Sigma_sq_expectation(sigma_star, γ)
+
+
+
+
+
          
