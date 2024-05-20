350d29d5e24e3f7323d6a4b14579c428f97eebd4
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2024-05-20 09:11:32 +0100

    changed quad gamma to be simpler

--- gamma_dist.py
+++ gamma_dist.py
@@ -97,6 +97,10 @@ class Gamma():
 
         # return M_3_vec  + (A_mat @ M_3_vec) + (B_mat @ M_2_vec)
 
+        std_devs = np.sqrt(np.diag(self.cov))
+        # print(std_devs, "std_devs")
+        self.corr = self.cov / np.outer(std_devs, std_devs)
+
         three_vec = np.zeros(self.dim)
 
         for i in range(self.dim):
@@ -126,35 +130,55 @@ class Gamma():
 
         quad_mat =  np.zeros((self.dim, self.dim))
 
-        col_vars = np.tile(np.diag(self.cov).reshape(1, self.dim), (self.dim, 1)) # if we change rows, should stay the same 
-        row_vars = col_vars.T # if we change cols, should stay the same 
+        # col_vars = np.tile(np.diag(self.cov).reshape(1, self.dim), (self.dim, 1)) # if we change rows, should stay the same 
+        # row_vars = col_vars.T # if we change cols, should stay the same 
 
-        A_mat  = self.cov / col_vars
-        np.fill_diagonal(A_mat, 0)
+        # A_mat  = self.cov / col_vars
+        # np.fill_diagonal(A_mat, 0)
 
-        A_sq_mat = A_mat**2
+        # A_sq_mat = A_mat**2
 
-        col_mu = np.tile(self.mean.reshape(1,self.dim), (self.dim,1))
-        row_mu = col_mu.T
+        # col_mu = np.tile(self.mean.reshape(1,self.dim), (self.dim,1))
+        # row_mu = col_mu.T
 
-        B_mat = 2 * A_mat * (row_mu - col_mu*A_mat)
-        np.fill_diagonal(B_mat, 0)
+        # B_mat = 2 * A_mat * (row_mu - col_mu*A_mat)
+        # np.fill_diagonal(B_mat, 0)
 
-        C_mat = (row_mu - A_mat * col_mu)**2 + row_vars * (1-self.corr**2)
-        np.fill_diagonal(C_mat, 0)
+        # C_mat = (row_mu - A_mat * col_mu)**2 + row_vars * (1-self.corr**2)
+        # np.fill_diagonal(C_mat, 0)
 
-        M_2_vec = np.diag(self.cov) + self.mean**2
-        M_3_vec = self.mean**3 + 3 * self.mean * np.diag(self.cov)
-        M_4_vec = self.mean**4 + 6 * self.mean**2 * np.diag(self.cov) + 3 * np.diag(self.cov)**2
+        # M_2_vec = np.diag(self.cov) + self.mean**2
+        # M_3_vec = self.mean**3 + 3 * self.mean * np.diag(self.cov)
+        # M_4_vec = self.mean**4 + 6 * self.mean**2 * np.diag(self.cov) + 3 * np.diag(self.cov)**2
+
+        # # diagonal terms
+
+        # np.fill_diagonal(quad_mat, M_4_vec + 4 * (A_sq_mat @ M_4_vec) + (B_mat @ M_3_vec) + (C_mat @ M_2_vec))
+
 
         # diagonal terms
 
-        np.fill_diagonal(quad_mat, M_4_vec + 4 * (A_sq_mat @ M_4_vec) + (B_mat @ M_3_vec) + (C_mat @ M_2_vec))
+        for i in range(self.dim):
+            quad_mat[i,i] = self.mean[i]**4 + 6 * self.mean[i]**2 * self.cov[i,i] + 3 * self.cov[i,i]**2
+                
+            for j in range(self.dim):
+                if j == i:
+                    continue
+
+                A_ij = self.corr[i,j]**2 * self.cov[i,i] / self.cov[j,j]
+
+                B_ij = 2.0 * self.corr[i,j] * (self.mean[i] - self.corr[i,j] * self.mean[j]* (np.sqrt(self.cov[i,i]) / np.sqrt(self.cov[j,j])))
+
+                C_ij = (self.mean[i] - np.sqrt(A_ij) * self.mean[j])**2 + self.cov[i,i] * (1 - self.corr[i,j]**2)
+
+                quad_mat[i,i] += A_ij * (self.mean[j]**4 + 6 * self.mean[j]**2 * self.cov[j,j] + 2 * self.cov[j,j]**2)
+                quad_mat[i,i] += B_ij * (self.mean[j]**3 + 3*self.mean[j]*self.cov[j,j])
+                quad_mat[i,i] += C_ij * (self.mean[j]**2 + self.cov[j,j])
 
 
         # off-diagonal terms
 
-        B_mat_off_diag = col_mu - (row_mu * (A_mat / row_vars))
+        # B_mat_off_diag = col_mu - (row_mu * (A_mat / row_vars))
 
         P_tensor = np.zeros((self.dim, self.dim, self.dim))
         Q_tensor = np.zeros((self.dim, self.dim, self.dim))
@@ -191,14 +215,27 @@ class Gamma():
                 if i == j:
                     continue
 
-                quad_mat[i,j] = A_mat[i,j]*M_4_vec[i] + B_mat_off_diag[i,j] * M_3_vec[i] + \
-                                A_mat[j,i]*M_4_vec[j] + B_mat_off_diag[j,i] * M_3_vec[j]
+                # quad_mat[i,j] = A_mat[i,j]*M_4_vec[i] + B_mat_off_diag[i,j] * M_3_vec[i] + \
+                #                 A_mat[j,i]*M_4_vec[j] + B_mat_off_diag[j,i] * M_3_vec[j]
+
+                A_ij = self.corr[i,j] * np.sqrt(self.cov[i,i] / self.cov[j,j])
+                A_ji = self.corr[j,i] * np.sqrt(self.cov[j,j] / self.cov[i,i])
+
+                B_ij = self.mean[j] - self.corr[i,j] * self.mean[i] * np.sqrt(self.cov[j,j] / self.cov[i,i])
+                B_ji = self.mean[i] - self.corr[j,i] * self.mean[j] * np.sqrt(self.cov[i,i] / self.cov[j,j])
+
+                quad_mat[i,j] = A_ij * (self.mean[i]**4 + 6 * self.mean[i]**2 * self.cov[i,i] + 3 * self.cov[i,i]**2) + \
+                                B_ij * (self.mean[i]**3 + 3 * self.mean[i] * self.cov[i,i]) + \
+                                A_ji * (self.mean[j]**4 + 6 * self.mean[j]**2 * self.cov[j,j] + 3 * self.cov[j,j]**2) + \
+                                B_ji * (self.mean[j]**3 + 3 * self.mean[j] * self.cov[j,j])
+
                 
                 for k in range(self.dim):
                     if k == i or k == j:
                         continue
 
-                    quad_mat[i,j] += P_tensor[i,j,k] * M_3_vec[k] + Q_tensor[i,j,k] * M_2_vec[k]
+                    quad_mat[i,j] += P_tensor[i,j,k] * (self.mean[k]**3 + 3 * self.mean[k]*self.cov[k,k]) + \
+                                    Q_tensor[i,j,k] * (self.mean[k]**2 + self.cov[k,k])
         
 
         return quad_mat
