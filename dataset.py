9b3cf6ab94934727c54826fef4fc984aa925494d
Dariyan Khan <dariyan.khan19@imperial.ac.uk>
2024-05-31 12:12:04 +0100

    nnew bivariate class doesnt crash

--- dataset.py
+++ dataset.py
@@ -20,15 +20,13 @@ from scipy.stats import beta
 
 class Dataset():
     
-    def __init__(self, adj_mat, emb_dim, K=None):
-        self.adj_mat = adj_mat
+    def __init__(self, embds, emb_dim=2, N=1000, K=2):
         self.d = emb_dim
-        self.N = len(adj_mat)
+        self.N = N
         self.K = K
-        self.embds = self.spectral_emb() # of size (N, self.d) where N is the number of samples
+        self.embds = embds  # of size (N, self.d) where N is the number
         # print("self.embds", self.embds)
         self.normed_embds = self.embds / np.linalg.norm(self.embds, axis=1)[:, np.newaxis]
-        self.best_k_means = None
 
         self.means_vars = [Mu(i, self.d) for i in range(self.K)]
         self.sigma_star_vars = [Sigma_Star(i, self.d) for i in range(self.K)]
@@ -39,123 +37,7 @@ class Dataset():
         self.phi_var = Phi(self.K)
 
 
-            
-    def spectral_emb(self):
-        eigvals, eigvecs = np.linalg.eig(self.adj_mat)
-        sorted_indexes = np.argsort(np.abs(eigvals))[::-1]
-        eigvals = eigvals[sorted_indexes]
-        eigvecs = eigvecs[:,sorted_indexes]
-        embedding_dim = self.d
-        eigvecs_trunc = eigvecs[:,:embedding_dim]
-        eigvals_trunc = np.diag(np.sqrt(np.abs(eigvals[:embedding_dim])))
-        spectral_embedding = eigvecs_trunc @ eigvals_trunc
-        return spectral_embedding
-    
-    def gaussian_mm_init(self):
 
-
-        gmm = GMM_Init(self.normed_embds, n_components=self.K)
-        self.gmm = gmm
-
-        mu_cov = gmm.mu_prior_cov_estimate()
-        gamma_cov = gmm.gamma_prior_cov_estimate()
-        scale_mat, dof = gmm.sigma_prior_params_estimate()
-
-        # initialise the z variables
-
-        # gmm.predict_proba(data)
-
-        for z_var, data in zip(self.z_vars, self.normed_embds):
-            data = data.reshape(1, -1)
-
-            predicted_probs = self.gmm.fitted_gmm.predict_proba(data)
-            predicted_probs = predicted_probs[0]
-            z_var.probs = predicted_probs
-            # z_var.probs = np.zeros(self.K) + 0.4 + np.random.uniform(-0.1, 0.1, self.K)
-            # z_var.probs[label] = 1.0
-            # z_var.probs = z_var.probs / sum(z_var.probs)
-        
-        
-        # initialise the mean gamma and sigma
-
-
-        for k in range(self.K):
-            self.means_vars[k].prior_cov = mu_cov
-            self.means_vars[k].mean = gmm.cluster_centres[k]
-            self.means_vars[k].cov =  mu_cov
-
-            #print(f"mu_cov {k} det:", np.linalg.det(mu_cov))
-
-            self.sigma_star_vars[k].prior_scale = scale_mat
-            self.sigma_star_vars[k].prior_dof = dof
-
-            self.sigma_star_vars[k].scale = gmm.sigma_star_estimates[k] * (dof - self.d)
-            self.sigma_star_vars[k].dof = max(dof, self.d +3) # gmm.sigma_star_inits[k] * (dof - self.d)
-
-            self.sigma_star_vars[k].nu = gmm.cluster_covs[k][-1,-1]
-
-            #print(f"scale_mat {k} det:", np.linalg.det(scale_mat))
-                
-            self.gamma_vars[k].prior_cov = gamma_cov
-            self.gamma_vars[k].mean = gmm.gamma_estimates[k]
-            self.gamma_vars[k].cov = gamma_cov
-
-            self.gamma_vars[k].nu = gmm.cluster_covs[k][-1,-1]
-
-            # print(f"gamma_cov {k} det:", np.linalg.det(gamma_cov))
-            # print("gamma cov:", gamma_cov)
-
-
-        # initialise the r variables
-
-        full_sigma_inv_estimates = [np.linalg.inv(cov_mat) for cov_mat in gmm.cluster_covs]
-
-        print("sigma inv estimates:", full_sigma_inv_estimates)
-
-        for i, (r_var, label) in enumerate(zip(self.r_vars, gmm.labels)):
-            curr_data = self.normed_embds[i]
-            r_var.alpha = 0.5 * np.matmul(curr_data.T, np.matmul(full_sigma_inv_estimates[label], curr_data))
-
-            beta_numerator = np.matmul(curr_data.T, np.matmul(full_sigma_inv_estimates[label], self.means_vars[label].mean))
-            beta_denom = np.matmul(curr_data.T, np.matmul(full_sigma_inv_estimates[label], curr_data))
-
-            r_var.beta = beta_numerator / beta_denom
-
-            r_var.update_moments()
-        
-        # initialise phi variables
-
-        for k in range(self.K):
-            # count number of labels in group k
-            num_labels = sum([1 for lab in gmm.labels if lab == k])
-            self.phi_var.conc[k] = num_labels + self.phi_var.prior_conc[k]
-
-
-
-    
-    def k_means_init(self, clusters_to_check=list(range(2, 11))):
-        # Initialize K centroids by randomly selecting K points from the dataset
-
-        best_silh_score = -10
-        best_num_clusters = 1
-        best_k_means = None
-
-        for n in tqdm(clusters_to_check, desc="Performing KMeans"):
-
-            kmeans = KMeans(n_clusters=n, random_state=42)  # random_state for reproducibility
-
-            kmeans_fitted = kmeans.fit(self.embds)
-
-            curr_silh_score = silhouette_score(self.embds, kmeans_fitted.predict(self.embds))
-
-            if curr_silh_score > best_silh_score:
-                best_silh_score = curr_silh_score
-                best_num_clusters = n
-                best_k_means = kmeans_fitted
-        
-        
-        return best_k_means, best_num_clusters
-    
     def dataset_vi(self, max_iter=1000, run_init=True):
 
         if run_init:
@@ -266,130 +148,24 @@ class Dataset():
                   """)
 
 
-    
-    def generate_best_k_means(self):
-
-        if self.K is None:
-            best_k_means, best_num_clusters = self.k_means_init(clusters_to_check=list(range(2, min(11, self.N))))
-            self.K = best_num_clusters
-        
-        else:
-            kmeans = KMeans(n_clusters=self.K, random_state=42)  # random_state for reproducibility
-            best_k_means = kmeans.fit(self.embds)
-        
-        self.best_k_means = best_k_means
-        
-
-
-class Synthetic_data(Dataset):
-
-    def __init__(self, μ_1, μ_2, prior, N_t=1000):
-        # prior has to be a function which takes no arguments and spits out a sample
-
-        # assert len(μ_1) == 2, "μ_1 and μ_2 have to be a 2D vector"
-        # assert len(μ_1) == len(μ_2), "μ_1 and μ_2 have to be a 2D vector"
-
-        self.μ_1 = μ_1
-        self.μ_2 = μ_2
-        self.mean_len = len(μ_1)
-        self.N_t=N_t
-        self.adj_mat, self.bern_params = self.simulate_adj_mat(prior, μ_1, μ_2)
-         # number of data points
-
-        super().__init__(self.adj_mat, emb_dim=self.mean_len, K=2)
-
-
-    def find_delta_inv(self, μ_1, μ_2, exp_rho):
-        μ_1_outer = np.outer(μ_1, μ_1)
-        μ_2_outer = np.outer(μ_2, μ_2)
-
-        Δ = exp_rho**2 * (1 / 2) * (μ_1_outer + μ_2_outer)
-
-        Δ_inv = np.linalg.inv(Δ) 
-        return Δ_inv
-
-    def exp_X1_inner_func(self, x, ρ, μ):
-        return (np.dot(x, ρ*μ) - (np.dot(x, ρ*μ)**2)) * np.outer(ρ*μ, ρ*μ)
-
-    def covariance_estimate(self, x, μ_1, μ_2, prior, exp_rho, N_ρ=1000, N_t=1000):
-        ρ_samples_1 = np.array([prior() for _ in range(N_ρ)])
-        ρ_samples_2 = np.array([prior() for _ in range(N_ρ)])
-        μ_1_integral_estimate = (1 / N_ρ) * sum(self.exp_X1_inner_func(x, ρ, μ_1) for ρ in ρ_samples_1)
-        μ_2_integral_estimate = (1 / N_ρ) * sum(self.exp_X1_inner_func(x, ρ, μ_2) for ρ in ρ_samples_2)
-        exp_X1_func_estimate = 0.5 * (μ_1_integral_estimate + μ_2_integral_estimate)
-        Δ_inv = self.find_delta_inv(μ_1, μ_2, exp_rho)
-        return (Δ_inv @ exp_X1_func_estimate @ Δ_inv) / N_t
-
-    def check_symmetric(self, a, rtol=1e-05, atol=1e-08):
-        return np.allclose(a, a.T, rtol=rtol, atol=atol)
-
-    def simulate_adj_mat(self, prior, μ_1, μ_2):
-        μ_mat = np.stack((μ_1, μ_2), axis=1)
-        # bern_params = [(prior(), np.random.randint(0,2)) for _ in range(self.N_t)]
-        bern_params = [(prior(), i % 2) for i in range(self.N_t)]
-        adj_mat = np.zeros((self.N_t, self.N_t))
-
-        for i in range(self.N_t):
-            ρ_i, μ_i = bern_params[i][0], μ_mat[:, bern_params[i][1]]
-            for j in range(i):
-                ρ_j, μ_j = bern_params[j][0], μ_mat[:, bern_params[j][1]]
-
-                adj_mat[i,j] = np.random.binomial(1, ρ_i * ρ_j * np.dot(μ_i, μ_j))
-
-                adj_mat[j,i] = adj_mat[i,j]
-            
-            adj_mat[i,i] = 1
-        
-        assert self.check_symmetric(adj_mat)
-
-        return adj_mat, bern_params
-    
-    def spectral_emb(self):
-        μ_mat = np.stack((self.μ_1, self.μ_2), axis=1)
-        eigvals, eigvecs = np.linalg.eig(self.adj_mat)
-        sorted_indexes = np.argsort(np.abs(eigvals))[::-1]
-        eigvals = eigvals[sorted_indexes]
-        eigvecs = eigvecs[:,sorted_indexes]
-        embedding_dim = self.d # should be 2
-        eigvecs_trunc = eigvecs[:,:embedding_dim]
-        eigvals_trunc = np.diag(np.sqrt(np.abs(eigvals[:embedding_dim])))
-        spectral_embedding = eigvecs_trunc @ eigvals_trunc
-        true_means = np.zeros((self.N_t, self.mean_len))
-
-        for i in range(self.N_t):
-            ρ_i, μ_i = self.bern_params[i][0], μ_mat[:, self.bern_params[i][1]]
-            true_means[i, :] =  ρ_i * μ_i
-
-        # print("true_means", true_means)
-
-        best_orthog_mat = orthogonal_procrustes(spectral_embedding, true_means)
-
-        spectral_embedding = spectral_embedding @ best_orthog_mat[0]
-
-        # print("spectral_embedding", spectral_embedding)
-
-        # print("min norm", min(spectral_embedding, key=np.linalg.norm))
-
-        return spectral_embedding
-
 
 if __name__ == '__main__':
 
-    μ_1 = np.array([0.75,0.25])
-    μ_2 = np.array([0.25, 0.75])
+    μ_0 = np.array([0.75,0.25])
+    μ_1 = np.array([0.25, 0.75])
 
     # μ_1 = μ_1 / np.linalg.norm(μ_1)
     # μ_2 = μ_2 / np.linalg.norm(μ_2)
 
-    μ_1 = np.array([0.75,0.25])
-    cov_1 = np.array([[0.1, 0.05], [0.05, 0.1]])
+    μ_0 = np.array([0.75,0.25])
+    cov_0 = np.array([[0.1, 0.05], [0.05, 0.1]])
 
-    μ_2 = np.array([0.25, 0.75])
-    cov_2 = np.array([[0.1, 0.05], [0.05, 0.1]])
+    μ_1 = np.array([0.25, 0.75])
+    cov_1 = np.array([[0.1, 0.05], [0.05, 0.1]])
 
     gamma_prior_cov = np.array([0.1])
 
-    ν = cov_1[1,1]
+    ν = cov_0[1,1]
 
 
     # Number of samples to generate
@@ -399,9 +175,47 @@ if __name__ == '__main__':
     samples = np.zeros((n_samples, 2))
     for i in range(n_samples):
         if i % 2 == 0:
-            samples[i] = np.random.multivariate_normal(μ_1, cov_1)
+            samples[i] = np.random.multivariate_normal(μ_1, cov_0)
         else:
-            samples[i] = np.random.multivariate_normal(μ_2, cov_2)
+            samples[i] = np.random.multivariate_normal(μ_1, cov_1)
+
+    
+    ds = Dataset(samples, emb_dim=2, N=1000, K=2)
+
+    assumed_dof = 5 #= d+3
+
+    ds.means_vars[0].prior_cov = cov_0
+    ds.means_vars[0].mean = μ_0
+    ds.means_vars[0].cov = cov_0
+
+    ds.means_vars[1].prior_cov = cov_1
+    ds.means_vars[1].mean = μ_1
+    ds.means_vars[1].cov = cov_1
+
+
+    ds.gamma_vars[0].prior_cov = np.array([gamma_prior_cov / np.sqrt(ν)])
+    ds.gamma_vars[0].mean = np.array([cov_0[0, 1]]) / np.sqrt(ν)
+    ds.gamma_vars[0].cov = np.array([gamma_prior_cov / ν])
+    ds.gamma_vars[0].nu = cov_0[-1,-1]
+
+    ds.gamma_vars[1].prior_cov = np.array([gamma_prior_cov / np.sqrt(ν)])
+    ds.gamma_vars[1].mean = np.array([cov_1[0, 1]]) / np.sqrt(ν)
+    ds.gamma_vars[1].cov = np.array([gamma_prior_cov / ν])
+    ds.gamma_vars[1].nu = cov_1[-1,-1]
+
+    ds.sigma_star_vars[0].prior_scale = np.array([cov_1[0,0] * (assumed_dof - ds.d)])
+    ds.sigma_star_vars[0].scale = np.array([cov_1[0,0] - ds.gamma_vars[0].mean ** 2])
+    ds.sigma_star_vars[0].nu = cov_1[-1,-1]                
+
+
+    ds.sigma_star_vars[1].prior_scale = np.array([cov_1[0,0] - ds.gamma_vars[0].mean ** 2])
+    ds.sigma_star_vars[1].scale = np.array([[cov_1[0,0] * (assumed_dof - ds.d)]])
+    ds.sigma_star_vars[1].nu = cov_1[-1,-1]
+
+
+
+
+
 
     
 
